55d094b4800bdbaff7b9e5d43eb14b0f528dce13
codeWithArif1982 <codewitharif1982@gmail.com>
2023-07-13 00:35:25 -0400

    Add Component folder with Footer.js file

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+  return <footer>
+    <h3>Arif Chowdhuary</h3>
+    <quote>Webdevelopment is fun</quote>
+  </footer>;
+}
+
+export default Footer;
